24d1d1a7e12d2ea82f93df5562f3910a59f31425
Vadim Vlasov <auxilant@gmail.com>
2020-04-12 15:09:44 +0300

    Add Dot / SquareDot classes

---
+++ GraphXDesign/Dot/Dot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace GraphXDesign
+{
+    abstract class Dot
+    {
+        int X { get; set; }
+        int Y { get; set; }
+        int Size { get; set; }
+        Color Colour { get; set; }
+
+        public Dot(int x, int y, int size, Color colour)
+        {
+            X = x;
+            Y = y;
+            Size = size;
+            Colour = colour;
+        }
+
+        abstract public void Draw(Bitmap bmp);
+    }
+}
---
+++ GraphXDesign/Dot/SquareDot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace GraphXDesign
+{
+    class SquareDot:Dot
+    {
+        public SquareDot(int x, int y, int size, Color colour) : base(x, y, size, colour) { }
+
+        public override void Draw(Bitmap bmp)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
