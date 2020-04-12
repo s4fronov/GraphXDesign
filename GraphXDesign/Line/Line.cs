c4d00793fe878daba66d7125ec49375fc9c0783b
Vadim Vlasov <auxilant@gmail.com>
2020-04-12 16:27:56 +0300

    Add Line class
    Line.Draw пока не соединяет две точки, просто прорисовывает обе

---
+++ GraphXDesign/Line/Line.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace GraphXDesign.Line
+{
+    public class Line
+    {
+        public Dot A { get; set; }
+        public Dot B { get; set; }
+
+        public Line(Dot a, Dot b)
+        {
+            A = a;
+            B = b;
+        }
+
+        //пока не соединяет точки, просто прорисовывает обе
+        public void Draw(Bitmap bmp)
+        {
+            A.Draw(bmp);
+            B.Draw(bmp);
+        }
+    }
+}
