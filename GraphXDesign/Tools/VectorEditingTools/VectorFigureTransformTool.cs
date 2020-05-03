3e943f70524740fef71c6091269c137ec1d90fca
Vadim Vlasov <auxilant@gmail.com>
2020-05-03 09:40:51 +0300

    Add adding points to transform tool
    тестовая версия, не очень хорошо работает, но работает

--- GraphXDesign/Tools/VectorEditingTools/VectorFigureTransformTool.cs
+++ GraphXDesign/Tools/VectorEditingTools/VectorFigureTransformTool.cs
@@ -41,6 +41,21 @@ namespace GraphXDesign
                     break;
                 }
             }
+
+            //для дабл клика(добавления вершин)
+            if (activeFigure == null)
+            {
+                cursorActive = false;
+                foreach (Drawfigure f in canvas.figures)
+                {
+                    if (f.figure.IsInside(e.Location))
+                    {
+                        activeFigure = f;
+                    }
+                }
+            }
+            //---------
+
             canvas.RenderExceptFigure(activeFigure);
             canvas.SaveToCache();
         }
@@ -68,14 +83,14 @@ namespace GraphXDesign
         }
         public void MouseDoubleClick(PictureBox sheet, IBrush brush, IFill fill, MouseEventArgs e)
         {
-            /*if (activeFigure != null)
+            if (activeFigure != null)
             {
                 canvas.LoadFromCache();
-                activeFigure.figure.AddPoint(e.Location, 5);
+                activeFigure.figure.AddPoint(e.Location, 10);
                 activeFigure.Draw(canvas);
                 canvas.PointChangeMode(sheet);
                 canvas.WriteToPictureBox(sheet);
-            }*/
+            }
         }
     }
 }
