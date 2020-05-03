4f6a2496bfcff79b4c293ab1912fdbb2415e92e4
Vadim Vlasov <auxilant@gmail.com>
2020-05-03 07:34:58 +0300

    Fix linedrawer

--- GraphXDesign/Draw/Drawfigure.cs
+++ GraphXDesign/Draw/Drawfigure.cs
@@ -23,6 +23,14 @@ namespace GraphXDesign
 
         public void Draw(AbstractCanvas canvas)
         {
+            //случай линии
+            if (figure.dotlist.Count == 2)
+            {
+                Drawline drawer = new Drawline(figure.dotlist[0].X, figure.dotlist[0].Y, figure.dotlist[1].X, figure.dotlist[1].Y, brush, true);
+                drawer.Draw(canvas);
+                return;
+            }
+
             BitmapWrap tmp = new BitmapWrap(canvas.Width, canvas.Height);
             tmp.Lock();
 
