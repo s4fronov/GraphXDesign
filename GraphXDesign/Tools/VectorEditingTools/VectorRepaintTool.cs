2ffdf45066a57528e948b853d08986ef57577897
Gulnara <ormanoag@ail.ru>
2020-05-04 10:21:17 +0300

    Change Repaint

--- GraphXDesign/Tools/VectorEditingTools/VectorRepaintTool.cs
+++ GraphXDesign/Tools/VectorEditingTools/VectorRepaintTool.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Drawing;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace GraphXDesign
 {
@@ -13,11 +7,17 @@ namespace GraphXDesign
         bool cursorActive;
         VectorCanvas canvas;
         Drawfigure activeFigure;
+        public IFigure figure;
+        public IBrush brush;
+        public IFill fill;
         public VectorRepaintTool()
         {
             cursorActive = false;
             canvas = VectorCanvas.GetCanvas;
             activeFigure = null;
+            //this.figure = (IFigure)figure.Clone();
+            //this.brush = (IBrush)brush.Clone();
+            //this.fill = (IFill)fill.Clone();
         }
         public void MouseDown(PictureBox sheet, IBrush brush, IFill fill, MouseEventArgs e)
         {
@@ -29,7 +29,7 @@ namespace GraphXDesign
                     cursorActive = true;
                     canvas.RenderExceptFigure(activeFigure);
                     canvas.SaveToCache();
-                    
+
                 }
             }
         }
@@ -39,9 +39,8 @@ namespace GraphXDesign
         }
         public void MouseUp(PictureBox sheet, IBrush brushFig, IFill fillFig, MouseEventArgs e)
         {
-            activeFigure.brush.BrushColor = brushFig.BrushColor;
-            activeFigure.brush.BrushSize = brushFig.BrushSize;
-            activeFigure.fill = fillFig;
+            activeFigure.brush = (IBrush)brushFig.Clone();
+            activeFigure.fill = (IFill)fillFig.Clone();
             cursorActive = false;
             canvas.Render();
             canvas.WriteToPictureBox(sheet);
